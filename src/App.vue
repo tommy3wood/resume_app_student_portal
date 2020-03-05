97be95c825f3de42e533ca81a33ca0a46821c39c
tommy3wood <thomaswcurran1014@gmail.com>
2020-03-05 16:10:57 -0600

    crud shell

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">Login</router-link> |
+      <router-link to="/logout">Logout</router-link> |
+      <router-link to="/show">Student Profile</router-link> |
+      <router-link to="/edit">Edit Student Profile</router-link>
     </div>
     <router-view/>
   </div>
--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,18 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Login from '../views/Login.vue'
+import Logout from '../views/Logout.vue'
+
+import Show from '../views/StudentShow.vue'
+import Edit from '../views/StudentEdit.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  {path: '/', name: 'login',component: Login},
+  {path: '/logout', name: 'logout',component: Logout},
+  {path: '/show', name: 'show',component: Show},
+  {path: '/edit', name: 'edit',component: Edit}
 ]
 
 const router = new VueRouter({
