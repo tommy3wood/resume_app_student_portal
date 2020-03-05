a96548bd5a21dd283512e6b692a0d509d7c368d0
“jamalnuman” <jnuman82@gmail.com>
2020-03-05 16:27:39 -0600

    completed the login/logout components

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,20 +1,59 @@
 <template>
-  <div class="home">
-    <h1>{{ message }}</h1>
+  <div class="login">
+    <div class="row">
+      <form class="col-lg-6 offset-lg-3 col-md-8 offset-md-2 col-sm-10 offset-sm-1" v-on:submit.prevent="submit()"> 
+        <h1 class="text-center mb-5">Login</h1>
+        <ul>
+          <li class="text-danger" v-for="error in errors">{{ error }}</li>
+        </ul>
+        <div class="form-group">
+          <label>Email:</label>
+          <input type="email" class="form-control" v-model="email">
+        </div>
+        <div class="form-group">
+          <label>Password:</label>
+          <input type="password" class="form-control" v-model="password">
+        </div>
+        <input type="submit" class="btn btn-primary" value="Submit">
+      </form>
+    </div>
   </div>
 </template>
 
-<style>
-</style>
-
 <script>
+import axios from "axios";
+
 export default {
   data: function() {
     return {
-      message: "Welcome to Vue.js!"
+      email: "",
+      password: "",
+      errors: []
     };
   },
-  created: function() {},
-  methods: {}
+  methods: {
+    submit: function() {
+      var params = {
+        email: this.email,
+        password: this.password
+      };
+      axios
+        .post("/api/sessions", params)
+        .then(response => {
+          axios.defaults.headers.common["Authorization"] = "Bearer " + response.data.jwt;
+          localStorage.setItem("jwt", response.data.jwt);
+
+          this.$parent.userEmail = response.data.email;
+          localStorage.setItem("userEmail", response.data.email);
+
+          this.$router.push("/"); 
+        })
+        .catch(error => {
+          this.errors = ["Invalid email or password"];
+          this.email = ""; 
+          this.password = ""; 
+        });
+    }
+  }
 };
 </script>
--- src/views/Logout.vue
+++ src/views/Logout.vue
@@ -1,20 +1,18 @@
 <template>
-  <div class="home">
-    <h1>{{ message }}</h1>
+  <div class="logout">
   </div>
 </template>
 
-<style>
-</style>
-
 <script>
+import axios from "axios";
+
 export default {
-  data: function() {
-    return {
-      message: "Welcome to Vue.js!"
-    };
-  },
-  created: function() {},
-  methods: {}
+  created: function() {
+    delete axios.defaults.headers.common["Authorization"];
+    localStorage.removeItem("jwt");
+    this.$parent.userEmail = "" 
+    localStorage.removeItem("userEmail")
+    this.$router.push("/");
+  }
 };
 </script>
